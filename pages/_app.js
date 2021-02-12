0d3204b05bac98224dab718de54dbfd4ed857d68
viktor_strelianyi <chmpain@gmail.com>
2021-02-12 20:13:50 +0200

    - udpate

--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,25 @@ import 'styles/index.scss';
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Footer, Navbar, Hero } from 'components/shared';
-import { RestfulProvider } from "restful-react";
+import { RestfulProvider } from 'restful-react';
 
-const App = ({Component, pageProps}) => {
+const App = ( { Component, pageProps, } ) => {
   const isHome = Component.name === 'Home';
   const Wrapper = Component.name === 'PortfolioDetail' ? React.Fragment : Container;
+
   return (
     <div className="portfolio-app">
       <Navbar />
       { isHome && <Hero /> }
       <Wrapper>
-        <Component {...pageProps} />
+        <Component { ...pageProps } />
       </Wrapper>
       { isHome && <Footer /> }
     </div>
-  )
-}
+  );
+};
 
-export default ({...props}) =>
+export default ( { ...props } ) =>
   <RestfulProvider base="http://localhost:3001/api">
-    <App {...props}/>
+    <App { ...props }/>
   </RestfulProvider>;
